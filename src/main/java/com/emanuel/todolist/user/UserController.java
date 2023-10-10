9c7df5e294b67d9f698df5293845a53d992b2e9d
Emanuel Tavecia <emanueltavecia@hotmail.com>
2023-10-10 18:27:15 -0300

    add database integration

---
+++ src/main/java/com/emanuel/todolist/user/UserController.java
@@ -0,0 +1,28 @@
+package com.emanuel.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+
+public class UserController {
+  @Autowired
+  private IUserRepository userRepository;
+
+  @PostMapping("/")
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) {
+      return ResponseEntity.status(400).body("User already exists");
+    }
+
+    var userCreated = this.userRepository.save(userModel);
+    return ResponseEntity.status(200).body(userCreated);
+  }
+}
