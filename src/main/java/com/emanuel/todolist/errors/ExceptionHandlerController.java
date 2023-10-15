2bf7efd111e89d9626e4e0637f058b8d12c7c924
Emanuel Tavecia <emanueltavecia@hotmail.com>
2023-10-14 21:16:27 -0300

    add new validations and dockerfile deploy

---
+++ src/main/java/com/emanuel/todolist/errors/ExceptionHandlerController.java
@@ -0,0 +1,14 @@
+package com.emanuel.todolist.errors;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<String> handleHttpMessageNoteReadableException(HttpMessageNotReadableException e) {
+    return ResponseEntity.status(400).body(e.getMostSpecificCause().getMessage());
+  }
+}
